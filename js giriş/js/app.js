ced766f7167bb4e72e10987404f7b67d229dfda5
Shahla Maharramova <sehlameherremova888@gmail.com>
2021-03-13 13:56:48 +0300

    2 ededin toplanmasi alqoritmi hazirdi

--- js giriş/js/app.js
+++ js giriş/js/app.js
@@ -1,11 +1,22 @@
 let a, b, c;
 a = prompt("Birinci ədədi daxil edin");
 
+if (!/^[0-9]+$/.test(a)) {
+  prompt("Zəhmət olmasa rəqəm daxil edin");
+}
 
-console.log(typeof a);
 b = prompt("İkinci ədədi daxil edin");
-
-c = a + b;
+if (!/^[0-9]+$/.test(b)) {
+  prompt("Zəhmət olmasa rəqəm daxil edin");
+}
+c = Number(a) + Number(b);
 console.log(c);
 // Alqoritm
-       m,,,,
+// 1.a b ve c deyerlirini elave edirik
+// 2.a dəyirini daxil edirik
+// 3 a deyerini reqem olubolmadigini yoxluyuruq
+// 4 a reqem deyilse yeniden sorgu gonderirirk
+// 5.b dəyirini daxil edirik
+// 6 b deyerini reqem olubolmadigini yoxluyuruq
+// 7 b reqem deyilse yeniden sorgu gonderirirk
+// 8 a ve b ededlerin topluyuruq
