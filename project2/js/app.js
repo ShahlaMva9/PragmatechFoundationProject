7a0240a35e04df8395cbe9fada00822da8a47822
Shahla Maharramova <sehlameherremova888@gmail.com>
2021-03-14 21:33:41 +0300

    Əsas səhifə bitdi

---
+++ project2/js/app.js
@@ -0,0 +1,13 @@
+document.getElementById("aboutMe").addEventListener("click", function () {
+  document.getElementById("about-me").style.display = "block";
+});
+document.getElementById("closeBtn").addEventListener("click", function () {
+  document.getElementById("about-me").style.display = "none";
+});
+
+document.getElementById("myPortfolio").addEventListener("click", function () {
+  document.getElementById("my-portfolio").style.display = "block";
+});
+document.getElementById("closeBtn").addEventListener("click", function () {
+  document.getElementById("my-portfolio").style.display = "none";
+});
