41b33220f594ce7e757cae6799a889255c44e55a
Shahla Maharramova <sehlameherremova888@gmail.com>
2021-05-05 21:41:09 +0300

    LAyihemdeki bir hissede admin panel tetbiq olundu

--- file structure/app/__init__.py
+++ file structure/app/__init__.py
@@ -1,12 +1,14 @@
-from admin.routes import *
-from main.routes import *
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
-
 app = Flask(__name__)
-
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
+UPLOAD_FOLDER = 'app/static/uploads/'
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+db = SQLAlchemy(app)
+# models
+from app.models import *
 # admin rouutes
-
+from admin.routes import *
 # main routes
+from main.routes import *
--- file structure/main/routes.py
+++ file structure/main/routes.py
@@ -1,6 +1,9 @@
 from app import app
+from app.models import Contact
+from flask import render_template, redirect, request
 
 
 @app.route("/")
 def main_index():
-    return "main page"
+    contacts=Contact.query.all()
+    return render_template("main/index.html", contacts=contacts)
